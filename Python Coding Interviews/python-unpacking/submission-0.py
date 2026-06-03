3e0f6a49b348931425d22be256446feaae51f29e
Mia Gomez <miafendi01@gmail.com>
2026-06-03 05:35:00 -0500

    Add: python-unpacking - submission-0

---
+++ Python Coding Interviews/python-unpacking/submission-0.py
@@ -0,0 +1,22 @@
+from typing import List, Tuple
+
+
+def sum_3_integers(triplet: List[int]) -> int:
+    x1, y1, z1 = triplet # we take triplet argument then make the 3 pts into x1 y1 z1
+    return x1 + y1 + z1
+
+
+
+def compute_volume(box_dimensions: Tuple[int, int, int]) -> int:
+    x1, y1, z1 = box_dimensions
+    return x1 * y1 * z1
+
+  
+
+# do not modify below this line
+print(sum_3_integers([1, 2, 3]))
+print(sum_3_integers([4, 6, 2]))
+
+print(compute_volume((1, 2, 3)))
+print(compute_volume((3, 2, 1)))
+print(compute_volume((3, 9, 7)))
